d1c0833430a9b39eaa68a2aa997b41494a67a994
k.kozhelin <k.kozhelin@gmail.com>
2022-09-30 18:48:50 +0300

    first commit

---
+++ js/filterComponent.js
@@ -0,0 +1,23 @@
+Vue.component('myfilter', {
+    template: `<form action="#" class="search-form" @submit.prevent="$root.filter">
+                     <input type="text" class="search-field" v-model="$root.userSearch">
+                     <button type="submit" class="btn-search">
+                         <i class="fas fa-search"></i>
+                     </button>
+            </form>`
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
